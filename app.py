c42eaf59823107aa570ad635b58684c3546406ef
prince-vizzy <bernad@example.com>
2025-08-07 23:19:41 +0300

    Debug login

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ db_config = {
     'user': os.getenv('DB_USER'),
     'password': os.getenv('DB_PASSWORD'),
     'database': os.getenv('DB_NAME'),
-    'port': 3306
+    'port': int(os.getenv('DB_PORT', 3306))
 }
 
 
